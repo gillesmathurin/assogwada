23c4c80c7340b7ac23a5a3fbcc44e2b3132a2332
Gilles MATHURIN <gillesmath@macmini.local>
2010-05-06 09:21:06 -0400

    Localisation de l'application

--- app/models/abonne.rb
+++ app/models/abonne.rb
@@ -1,6 +1,6 @@
 class Abonne < ActiveRecord::Base
   validates_presence_of :nom_abonne, :email_abonne, :on => :create, :message => "ne peut être vide"
   validates_format_of :email_abonne, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-   :on => :create, :message => "n'est pas valide"
+   :on => :create, :message => "l'adresse email n'est pas valide"
   validates_uniqueness_of :email_abonne, :on => :create, :message => "cet adresse email est déjà enregistrée"
 end
