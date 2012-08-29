ed2d390886239ab78ac366981b2c3f0e6cfdb556
Gilles Mathurin <gillesmath@me.com>
2012-08-29 08:32:47 -0400

    added necessary configuration for devise mailer and localized the reset_password mailer view

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
 Assogwada::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
